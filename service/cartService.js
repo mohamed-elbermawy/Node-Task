928eb00316d84ed8d26e894b7d6f1c0e1a4672be
mohamed-elbermawy <mohamed77bermawy@gmail.com>
2021-06-16 17:00:25 +0200

    updating in cart Service

--- service/cartService.js
+++ service/cartService.js
@@ -13,8 +13,6 @@ module.exports.addToCart = async (data) => {
     if (checkProduct.qty < data.qty) {
       error.available = "No available stock";
       return error;
-    } else {
-      checkProduct.qty = checkProduct.qty - data.qty;
     }
 
     let cart = new Cart();
@@ -23,18 +21,12 @@ module.exports.addToCart = async (data) => {
       quantityToBuy: data.qty,
     });
     const result = await cart.save();
-    const updatedProduct = await Product.updateOne(
-      { _id: data.productID },
-      checkProduct
-    );
+
     if (!result) {
       error.internal = "Internal Server error";
       return error;
     }
-    if (!updatedProduct) {
-      error.internal = "Internal Server error";
-      return error;
-    }
+
     return result;
   } catch (err) {
     error.internal = err;
